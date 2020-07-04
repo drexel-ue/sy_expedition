52a8503822dfd33d423731d7d38901e766d82099
drexel-ue <packappsforyou@gmail.com>
2020-07-04 18:28:06 -0400

    Update constants

---
+++ lib/asset_constants.dart
@@ -0,0 +1,11 @@
+const leopard = 'assets/leopard.png';
+
+const leopards = 'assets/leopards.png';
+
+const map = 'assets/map.png';
+
+const mapMin = 'assets/map-min.png';
+
+const vulture = 'assets/vulture.png';
+
+const vultures = 'assets/vultures.png';
