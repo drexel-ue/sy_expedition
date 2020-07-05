90950395319fcde0c395a600415d199aea5c21f4
drexel-ue <packappsforyou@gmail.com>
2020-07-05 01:36:55 -0400

    Place travel details

--- lib/page_2.dart
+++ lib/page_2.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sy_expedition/color_constants.dart';
@@ -8,12 +10,18 @@ class Page2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final size = MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
 
-    return Column(
-      children: [
-        _HolyGlow(),
-      ],
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: size.width * .1),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _HolyGlow(),
+          SizedBox(height: size.height * .21),
+          _TravelDetails(),
+        ],
+      ),
     );
   }
 }
@@ -27,21 +35,48 @@ class _HolyGlow extends StatelessWidget {
 
     return Consumer<PageOffsetNotifier>(
       builder: (_, PageOffsetNotifier value, __) {
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(height: size.height * 0.21 + (size.width * .55 - (size.width * .55 * value.page)) / 2),
-            Container(
-              height: size.width * .55 * value.page,
-              width: size.width * .55 * value.page,
-              decoration: const BoxDecoration(
-                color: lightGrey,
-                borderRadius: BorderRadius.all(Radius.circular(999)),
+        return SizedBox(
+          height: size.height * 0.21 + size.width * .55,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: size.height * 0.21 + (size.width * .55 - (size.width * .55 * value.page)) / 2),
+              Container(
+                height: size.width * .55 * value.page,
+                width: size.width * .55 * value.page,
+                decoration: const BoxDecoration(
+                  color: lightGrey,
+                  borderRadius: BorderRadius.all(Radius.circular(999)),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
+    );
+  }
+}
+
+class _TravelDetails extends StatelessWidget {
+  const _TravelDetails({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PageOffsetNotifier>(
+      builder: (BuildContext context, PageOffsetNotifier value, Widget child) {
+        return Opacity(
+          opacity: math.max<double>(0, 4 * value.page - 3),
+          child: child,
+        );
+      },
+      child: Text(
+        'Travel details',
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: 18,
+          color: white,
+        ),
+      ),
     );
   }
 }
