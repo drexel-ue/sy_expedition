56af0eed0dcde7bd34213c0a9051d8b00cf47d65
drexel-ue <packappsforyou@gmail.com>
2020-07-04 21:54:11 -0400

    Add parallax to 72

--- lib/page_1.dart
+++ lib/page_1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sy_expedition/page_offset_notifier.dart';
 
 class Page1 extends StatelessWidget {
   const Page1({Key key}) : super(key: key);
@@ -7,25 +9,33 @@ class Page1 extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          height: height * .155,
-        ),
-        const RotatedBox(
-          quarterTurns: 1,
-          child: Text(
-            '72',
-            style: TextStyle(
-              fontSize: 360,
-              fontWeight: FontWeight.bold,
-              height: 1.3,
-              letterSpacing: -5,
+    return Consumer<PageOffsetNotifier>(
+      builder: (BuildContext context, PageOffsetNotifier value, Widget child) {
+        return Transform.translate(
+          offset: Offset(-value.offset, 0),
+          child: child,
+        );
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: height * .155,
+          ),
+          const RotatedBox(
+            quarterTurns: 1,
+            child: Text(
+              '72',
+              style: TextStyle(
+                fontSize: 360,
+                fontWeight: FontWeight.bold,
+                height: 1.3,
+                letterSpacing: -5,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
